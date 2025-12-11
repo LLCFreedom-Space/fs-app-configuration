59b26d701f09c71869d246db474b0717dee539a2
Mykola Buhaiov <gulivero1773@icloud.com>
2025-12-11 12:09:40 +0200

    fix: downgrade swift-tools-version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "fs-app-configuration",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v13)
     ],
     products: [
         .library(name: "AppConfiguration", targets: ["AppConfiguration"]),
@@ -15,7 +15,7 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.110.2"),
         // 🔏 Vapor JWT provider
-        .package(url: "https://github.com/vapor/jwt.git", from: "5.0.0")
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
